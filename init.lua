8515aa5c99734407e05236a3b246b0c8b8d3dce0
xianyang <xianyang.lu@vcg.com>
2023-08-30 17:48:24 +0800

    ctrl-p config

--- init.lua
+++ init.lua
@@ -9,11 +9,13 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
 
--- HopWord
+-- easy motion
 vim.api.nvim_set_keymap("n", ",em", ":HopWord<cr>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap("n", ",wo", ":only<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap("n", ",wc", ":close<cr>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap("n", ",bo", ":BufOnly<cr>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap("n", ",ss", ":luafile ~/.config/nvim/lua/custom/init.lua<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope find_files<cr>", { silent = true, noremap = true })
 
 vim.cmd [[
 command! BufOnly %bd | e#
