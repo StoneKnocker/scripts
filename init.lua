56bcdbc4339e054da07ce5b4a4b8caa62b24c4d9
xianyang <xianyang.lu@vcg.com>
2023-08-31 11:46:13 +0800

    rest.vim config

--- init.lua
+++ init.lua
@@ -20,8 +20,11 @@ vim.api.nvim_set_keymap("n", ",bo", ":BufOnly<cr>", { silent = true, noremap = t
 vim.api.nvim_set_keymap("n", ",ss", ":luafile ~/.config/nvim/lua/custom/init.lua<cr>", { noremap = true })
 -- use ctrl-p to find files
 vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope find_files<cr>", { silent = true, noremap = true })
+-- rest.vim
+vim.api.nvim_set_keymap("n", ",rnr", "<Plug>RestNvim", { silent = true, noremap = true })
+vim.api.nvim_set_keymap("n", ",rnp", "<Plug>RestNvimPreview", { silent = true, noremap = true })
+
 
 vim.cmd [[
 command! BufOnly %bd | e#
   ]]
-
